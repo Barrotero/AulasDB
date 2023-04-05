74eef95f220cee7218f864b956c318a3e4193d0b
David Carvalho de Oliveira <davidcarvalho_2@outlook.com>
2023-04-05 01:04:30 -0300

    Add files via upload

---
+++ Aula 5/Usabilidade/enviar_cadastro.php
@@ -0,0 +1,26 @@
+<?php
+// Conectar ao banco de dados SQL Server de origem
+$pdo1 = new PDO('mysql:host=localhost;dbname=fpb', 'root', '');
+
+// Conectar ao banco de dados PostgreSQL de destino
+$pdo2 = new PDO('pgsql:host=alunosfpb.postgresql.dbaas.com.br;dbname=alunosfpb;port=5432', 'alunosfpb', 'Alunos@2023');
+
+// Executar uma consulta no banco de dados SQL Server de origem
+$conexao = mysqli_connect('localhost', 'root', '', 'fpb');
+$result = $conexao->query("SELECT * FROM cadastro");
+
+// Loop pelos resultados e inserir no banco de dados PostgreSQL de destino
+foreach ($result as $row) {
+    $coluna1 = $row['matricula'];
+    $coluna2 = $row['nome'];
+    $coluna3 = $row['cpf'];
+    $coluna4 = $row['funcao'];
+
+    $stmt = $pdo2->prepare("INSERT INTO cadastro (matricula, nome, cpf, funcao) VALUES (?, ?, ?, ?)");
+    $stmt->execute([$coluna1, $coluna2, $coluna3, $coluna4]);
+}
+
+// Fechar as conexões com os bancos de dados
+$pdo1 = null;
+$pdo2 = null;
+?>
