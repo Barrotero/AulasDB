c6aca664fa8cf1430ed744180709d56baa968b14
David Carvalho de Oliveira <davidcarvalho_2@outlook.com>
2023-04-05 01:36:49 -0300

    Update enviar_cadastro.php

--- Aula 5/Usabilidade/enviar_cadastro.php
+++ Aula 5/Usabilidade/enviar_cadastro.php
@@ -1,14 +1,14 @@
 <?php
-// Conectar ao banco de dados SQL Server de origem
+// Conectar ao banco de dados SQL Server
 $pdo1 = new PDO('mysql:host=localhost;dbname=fpb', 'root', '');
 
-// Conectar ao banco de dados PostgreSQL de destino
+// Conectar ao banco de dados PostgreSQL
 $pdo2 = new PDO('pgsql:host=alunosfpb.postgresql.dbaas.com.br;dbname=alunosfpb;port=5432', 'alunosfpb', 'Alunos@2023');
 
-// Executar uma consulta no banco de dados SQL Server de origem
+// Executar uma consulta no banco de dados SQL
 $result = $pdo1->query("SELECT * FROM cadastro");
 
-// Loop pelos resultados e inserir no banco de dados PostgreSQL de destino
+// Loop pelos resultados, inserir e verificar inclusão de dados no banco de dados PostgreSQL
 foreach ($result as $row) {
     $coluna1 = $row['matricula'];
     $coluna2 = $row['nome'];
@@ -17,9 +17,15 @@ foreach ($result as $row) {
 
     $stmt = $pdo2->prepare("INSERT INTO cadastro (matricula, nome, cpf, funcao) VALUES (?, ?, ?, ?)");
     $stmt->execute([$coluna1, $coluna2, $coluna3, $coluna4]);
+
+    $verificar_cadastro = $stmt->rowCount();
+    if ($verificar_cadastro > 0) {
+        echo "Dados inseridos com sucesso no banco de dados PostgreSQL";
+        } else {
+        echo "Falha ao inserir os dados no banco de dados";
+    }   
 }
 
-// Fechar as conexões com os bancos de dados
 $pdo1 = null;
 $pdo2 = null;
 ?>
